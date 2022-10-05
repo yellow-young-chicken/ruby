fc518b348618aac53d592c2748246f10b87eac81
EC2 Default User <ec2-user@ip-172-31-43-36.ap-northeast-1.compute.internal>
2022-10-05 11:05:34 +0000

    [Finish] 9

---
+++ greeting.rb
@@ -0,0 +1,6 @@
+def greeting (name)
+  return"Hello,#{name}!"
+  "Good mornig,#{name}!"
+end
+
+puts greeting('John')
---
+++ new.rb
@@ -0,0 +1,42 @@
+# インスタンスメソッド
+class Car
+
+  def move(direction,distance)
+    self.turn(direction)
+    self.run(distance)
+  end
+
+
+  def turn(direction)
+    puts"#{direction}に曲がります"
+  end
+
+  def run(distance)
+    puts"車で#{distance}キロで走ります。"
+  end
+
+end
+
+car=Car.new
+car.move("右",5)
+
+
+# クラスメソッド
+
+class Car
+  def self.run(distance)
+    puts"車で#{distance}キロ走ります。"
+  end
+end
+
+
+Car.run(10)
+
+class Car
+  def self.turn(direction)
+    puts"#{direction}に曲がります。"
+  end
+end
+
+Car.turn("右")
+
